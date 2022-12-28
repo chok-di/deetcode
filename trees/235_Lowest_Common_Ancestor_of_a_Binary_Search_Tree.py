8671b705885a23badbac902c100aa5213b7a8b8b
Di Wu <dwu233@gmail.com>
2022-12-28 16:32:09 -0500

    tree

---
+++ trees/235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
@@ -0,0 +1,12 @@
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        curr = root
+        pval,qval = p.val,q.val
+        while curr:
+            if curr.val > max(pval,qval):
+                curr = curr.left
+                continue
+            elif curr.val < min(pval,qval):
+                curr = curr.right
+                continue
+            return curr
