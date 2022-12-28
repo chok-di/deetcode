57febad7f9932df1ed7bd856c8c850c0e94b2154
Di Wu <dwu233@gmail.com>
2022-12-28 17:26:40 -0500

    tree traversal problme

--- trees/102_Binary_Tree_Level_Order_Traversal.py
+++ trees/102_Binary_Tree_Level_Order_Traversal.py
@@ -1,15 +1,4 @@
-class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        curr = root
-        pval,qval = p.val,q.val
-        while curr:
-            if curr.val > max(pval,qval):
-                curr = curr.left
-                continue
-            elif curr.val < min(pval,qval):
-                curr = curr.right
-                continue
-            return curr# Definition for a binary tree node.
+# Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
