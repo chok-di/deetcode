2b5f395a569c4e707d9394e89d769fc0585affbe
Di Wu <dwu233@gmail.com>
2022-12-28 17:27:06 -0500

    delete duplicated code

--- trees/235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
+++ trees/235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
@@ -10,28 +10,3 @@ class Solution:
                 curr = curr.right
                 continue
             return curr# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root:
-            return []
-        q = collections.deque()
-        res = []
-        q.append(root)
-        while q:
-            val = []
-            width = len(q)
-            for i in range(width):
-                curr = q.popleft()
-                val.append(curr.val)
-                if curr.left:
-                    q.append(curr.left)
-                if curr.right:
-                    q.append(curr.right)
-            res.append(val)
-        return res
-            
